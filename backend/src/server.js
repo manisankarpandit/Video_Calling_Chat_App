c7f7193128c0353d4772ff3d53119ce467ffa78a
Mani Sankar Pandit <msankarpandit@gmail.com>
2026-09-24 20:52:54 +0530

    add

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,7 +17,7 @@ const __dirname = path.resolve();
 
 app.use(
   cors({
-    // origin: "http://localhost:5173",
+    origin: "http://localhost:5173",
     origin: "https://video-calling-chat-app-1-fhzr.onrender.com",
     credentials: true, // allow frontend to send cookies
   })
